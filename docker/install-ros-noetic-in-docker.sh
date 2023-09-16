68d9ed3f685a7148d8a201b8bc6861f2b6fc4c7a
WhateverMO <2447583173@qq.com>
2023-09-16 20:04:09 +0800

    this should work

--- docker/install-ros-noetic-in-docker.sh
+++ docker/install-ros-noetic-in-docker.sh
@@ -98,7 +98,7 @@ read -p "Do you want to install ros noetic use the install script? [y/n]" answer
 #Default answer is y
 answer=${answer:-y}
 if [ $answer == "y" ]; then
-	wget -c https://raw.githubusercontent.com/WhateverMO/eie/master/docker/install-ros-noetic-in-docker.sh -O install_noetic.sh && chmod +x ./install_noetic.sh && ./install_noetic.sh
+	wget -c https://raw.githubusercontent.com/qboticslabs/ros_install_noetic/master/ros_install_noetic.sh && chmod +x ./ros_install_noetic.sh && ./ros_install_noetic.sh
 fi
 
 
