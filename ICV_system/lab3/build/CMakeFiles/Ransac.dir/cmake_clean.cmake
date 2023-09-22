6870e0569074e0d2a96f93d0a973acf38545eec3
WhateverMO <2447583173@qq.com>
2023-09-22 11:47:55 +0800

    bacis finish lab3

---
+++ ICV_system/lab3/build/CMakeFiles/Ransac.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ransac.dir/Ransac.cpp.o"
+  "Ransac"
+  "Ransac.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ransac.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
