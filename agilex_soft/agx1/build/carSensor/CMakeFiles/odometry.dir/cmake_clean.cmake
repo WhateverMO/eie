a74ebd57b2895c19b51fab74d11beb0236a530e7
WhateverMO <2447583173@qq.com>
2023-09-25 22:16:08 +0800

    roscar agilex first init

---
+++ agilex_soft/agx1/build/carSensor/CMakeFiles/odometry.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/odometry.dir/src/odometry.cpp.o"
+  "/home/agilex/Documents/workspace/eie/agilex_soft/agx1/devel/lib/carSensor/odometry.pdb"
+  "/home/agilex/Documents/workspace/eie/agilex_soft/agx1/devel/lib/carSensor/odometry"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/odometry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ agilex_soft/agx1/build/carSensor/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/agilex/Documents/workspace/eie/agilex_soft/agx1/src/carSensor
+# Build directory: /home/agilex/Documents/workspace/eie/agilex_soft/agx1/build/carSensor
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
