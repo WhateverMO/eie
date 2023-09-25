9880c8da4c35397f4a59eb32a5f3c7d7820ac69c
WhateverMO <2447583173@qq.com>
2023-09-25 16:46:59 +0800

    car finish lab2 melodic

---
+++ ICV_system/lab2_melodic_on_car/build/carPCL/CMakeFiles/passthrough.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/passthrough.dir/src/passthrough.cpp.o"
+  "/home/agilex/Documents/workspace/eie/ICV_system/lab2_melodic_on_car/devel/lib/carPCL/passthrough.pdb"
+  "/home/agilex/Documents/workspace/eie/ICV_system/lab2_melodic_on_car/devel/lib/carPCL/passthrough"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/passthrough.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ICV_system/lab2_melodic_on_car/build/gtest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/agilex/Documents/workspace/eie/ICV_system/lab2_melodic_on_car/build/gtest/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
